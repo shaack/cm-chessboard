1f766ca9e135d8d7e376a675c2e791773afccb87
shaack <shaack@gmail.com>
2017-11-30 01:22:22 +0100

    Heavy refactoring

--- src/cm-chessboard/Chessboard.js
+++ src/cm-chessboard/Chessboard.js
@@ -5,54 +5,74 @@
 import {ChessboardView} from "./ChessboardView.js";
 import {ChessboardModel} from "./ChessboardModel.js";
 
+
+export const COLOR = {
+    white: "white",
+    black: "black"
+};
+
+export const MOVE_MODE = {
+    live: "live",
+    pbm: "pbm"
+};
+
+export const startPositionFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
 export class Chessboard {
 
     constructor(containerElement, config = {}) {
         this.config = {
-            initialPosition: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            initialOrientation: 'white', // white on bottom
+            position: startPositionFen,
+            orientation: COLOR.white, // white on bottom
+            showNotation: false, // TODO
             sprite: "../assets/sprite.svg", // figures and markers
             spriteGrid: 40, // one figure every 40 px
             responsive: false, // detect window resize
-            interactiveMoveMode: MOVE_MODE_LIVE, // type of interactive movement with mouse or tap
+            // moveInputMode: MOVE_MODE.pbm, // type of interactive movement with mouse or tap
             onBeforeMove: null, // callback, before interactive move, return true for ok
             onAfterMove: null // callback after interactive move
         };
         Object.assign(this.config, config);
-        this.model = new ChessboardModel(this.config.initialPosition, this.config.initialOrientation);
+        this.model = new ChessboardModel();
         this.view = new ChessboardView(containerElement, this.model, this.config, () => {
-            this.init();
+            this.position = this.config.position;
+            this.orientation = this.config.orientation;
+            // this.model.moveInputMode = this.config.moveInputMode;
+            this.view.redraw(); // TODO remove and redraw on observer
         });
-        this.interactiveMoveWhite = false;
-        this.interactiveMoveBlack = false;
-        this.interactiveMoveMode = config.interactiveMoveMode;
+    }
+
+    // API
+
+    set position(fen) {
+        this.model.parseFen(fen);
+    }
+
+    get position() {
+        return this.model.createFen();
+    }
+
+    set orientation(color) {
+        this.model.orientation = color;
+    }
+
+    get orientation() {
+        return this.model.orientation;
     }
 
     /**
-     * reset board and draw initial position
+     * Enables moves via user input, mouse or touch
+     * @param color
+     * @param enable
      */
-    init() {
-        this.view.redraw(this.model);
+    /* TODO think about it again
+    setEnableMoveFigures(color, enable) {
+        if (color === COLOR.white) {
+            this.model.moveInputWhiteEnabled = enable;
+        } else if (color === COLOR.black) {
+            this.model.moveInputBlackEnabled = enable;
+        }
     }
+    */
 
-    /**
-     * Enables move via user input
-     * @param enableWhite
-     * @param enableBlack
-     */
-    setInteractiveMove(enableWhite, enableBlack) {
-        this.interactiveMoveWhite = enableWhite;
-        this.interactiveMoveBlack = enableBlack;
-    }
-
-    /**
-     * The move mode controls how the interaction is displayed
-     * @param mode
-     */
-    setInteractiveMoveMode(mode) {
-        this.interactiveMoveMode = mode;
-    }
 }
-
-export const MOVE_MODE_LIVE = 1;
-export const MOVE_MODE_PBM = 2;
