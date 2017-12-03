7b200c913b64b39957177d77b551720488e1b7f4
shaack <shaack@gmail.com>
2017-12-03 22:04:06 +0100

    tests for setPosition and getPosition

--- test/TestPosition.js
+++ test/TestPosition.js
@@ -7,15 +7,23 @@ import {Test} from "../node_modules/svjs-test/src/svjs/Test.js"
 import {Chessboard} from "../src/cm-chessboard/Chessboard.js";
 
 export class TestPosition extends Test {
-    testSetAndGetPosition() {
-        const chessboard = new Chessboard(document.getElementById("testboard1"), {
-            // position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11"
-        });
-        // setTimeout(() => {
-            chessboard.setPosition("rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11");
-            const positionFromGet = chessboard.getPosition();
-        // });
 
-        // Test.assertEquals("rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR", positionFromGet);
+    testSetAndGetStartPosition() {
+        const chessboard = new Chessboard(document.getElementById("testboard1"), {
+            position: "start"
+        }, () => {
+            Test.assertEquals("wq", chessboard.getSquare("d1"));
+            Test.assertEquals("bq", chessboard.getSquare("d8"));
+            Test.assertEquals("wp", chessboard.getSquare("a2"));
+            Test.assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", chessboard.getPosition());
+        });
     }
+
+    testSetAndGetPosition() {
+        const chessboard = new Chessboard(document.getElementById("testboard1"), null, () => {
+            Test.assertEquals("rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR", chessboard.getPosition());
+        });
+        chessboard.setPosition("rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR w Gkq - 4 11")
+    }
+
 }
