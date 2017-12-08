cc209de603d59ebd950eefba37aa343282078b7f
shaack <shaack@gmail.com>
2017-12-08 02:01:12 +0100

    Test squareDistance and seekChanges

--- test/TestFigureAnimation.js
+++ test/TestFigureAnimation.js
@@ -5,15 +5,50 @@
 
 import {Test} from "../node_modules/svjs-test/src/svjs/Test.js"
 import {ChessboardFigureAnimation} from "../src/cm-chessboard/ChessboardFigureAnimation.js";
+import {ChessboardModel} from "../src/cm-chessboard/ChessboardModel.js";
+
+const cfa = ChessboardFigureAnimation;
 
 export class TestFigureAnimation extends Test {
     testSquareDistance() {
-        const cfa = new ChessboardFigureAnimation();
-        Test.assertEquals(0, cfa._squareDistance(0, 0));
-        Test.assertEquals(1, cfa._squareDistance(0, 1));
-        Test.assertEquals(7, cfa._squareDistance(0, 7));
-        Test.assertEquals(1, cfa._squareDistance(0, 8));
-        Test.assertEquals(2, cfa._squareDistance(10, 20));
-        Test.assertEquals(7, cfa._squareDistance(0, 63));
+        Test.assertEquals(0, cfa.squareDistance(0, 0));
+        Test.assertEquals(1, cfa.squareDistance(0, 1));
+        Test.assertEquals(7, cfa.squareDistance(0, 7));
+        Test.assertEquals(1, cfa.squareDistance(0, 8));
+        Test.assertEquals(2, cfa.squareDistance(10, 20));
+        Test.assertEquals(7, cfa.squareDistance(0, 63));
+        Test.assertEquals(2, cfa.squareDistance(8, 24));
+        Test.assertEquals(6, cfa.squareDistance(14, 24));
+    }
+    testSeekChanges() {
+        const model1 = new ChessboardModel();
+        model1.setPosition("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+        const model2 = new ChessboardModel();
+        model2.setPosition("rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR");
+
+        const previousBoard1 = model1.squares;
+        const newBoard1 = model2.squares;
+
+        const changes = cfa.seekChanges(previousBoard1, newBoard1);
+        console.log(previousBoard1, newBoard1, changes);
+
+        Test.assertEquals(1, changes[0].type);
+        Test.assertEquals("wn", changes[0].figure);
+        Test.assertEquals(1, changes[0].fromIndex);
+        Test.assertEquals(3, changes[0].toIndex);
+
+        Test.assertEquals(1, changes[2].type);
+        Test.assertEquals("wn", changes[2].figure);
+        Test.assertEquals(6, changes[2].fromIndex);
+        Test.assertEquals(18, changes[2].toIndex);
+
+        Test.assertEquals(1, changes[4].type);
+        Test.assertEquals("wp", changes[4].figure);
+        Test.assertEquals(8, changes[4].fromIndex);
+        Test.assertEquals(24, changes[4].toIndex);
+
+        Test.assertEquals(3, changes[13].type);
+        Test.assertEquals("bq", changes[13].figure);
+        Test.assertEquals(59, changes[13].atIndex);
     }
 }
