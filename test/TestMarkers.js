e3f4940d9bbed5eb9997304729a7c6a83a3c3bb5
shaack <shaack@gmail.com>
2017-12-16 18:14:56 +0100

    more testing

--- test/TestMarkers.js
+++ test/TestMarkers.js
@@ -12,14 +12,17 @@ export class TestMarkers extends Test {
             position: "empty"
         }, () => {
             chessboard.addMarker("e5");
-            Test.assertEquals(1, chessboard.getMarkers().length);
+            chessboard.addMarker("b6");
+            chessboard.addMarker("h6", MARKER_TYPE.lastMove);
+            Test.assertEquals(3, chessboard.getMarkers().length);
             Test.assertEquals(1, chessboard.getMarkers("e5").length);
             Test.assertEquals("e5", chessboard.getMarkers("e5")[0].square);
             Test.assertEquals("marker2", chessboard.getMarkers("e5")[0].type.slice);
-            Test.assertEquals(1, chessboard.getMarkers(null, MARKER_TYPE.emphasize).length);
+            Test.assertEquals(2, chessboard.getMarkers(null, MARKER_TYPE.emphasize).length);
             Test.assertEquals(1, chessboard.getMarkers("e5", MARKER_TYPE.emphasize).length);
             Test.assertEquals(0, chessboard.getMarkers("a4").length);
-            Test.assertEquals(0, chessboard.getMarkers(null, MARKER_TYPE.lastMove).length);
+            Test.assertEquals(1, chessboard.getMarkers(null, MARKER_TYPE.lastMove).length);
+            Test.assertEquals(0, chessboard.getMarkers("b6", MARKER_TYPE.newMove).length);
             chessboard.destroy();
         });
     }
