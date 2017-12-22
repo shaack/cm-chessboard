8a47de4fb5c7cf4d416757545f07b98781ac19cd
shaack <shaack@gmail.com>
2017-12-22 15:01:18 +0100

    removed copy assets

--- postinstall.js
+++ postinstall.js
@@ -9,6 +9,7 @@ process.chdir('./src');
 symlinkModule("svjs-svg");
 
 // copy assets
+/*
 if (resolveModulePath("cm-chessboard") !== null) {
     try {
         fs.mkdir(projectRoot + "/assets");
@@ -20,12 +21,10 @@ if (resolveModulePath("cm-chessboard") !== null) {
     } catch (e) {
         console.log(e.mesaage);
     }
-    /*
-    fs.copyFileSync(resolveModulePath("cm-chessboard") + "/assets/images/chessboard-sprite.svg",
-        projectRoot + "/assets/images/");
-        */
+    // fs.copyFileSync(resolveModulePath("cm-chessboard") + "/assets/images/chessboard-sprite.svg",
+    //    projectRoot + "/assets/images/");
 }
-
+*/
 function symlinkModule(moduleName) {
     try {
         fs.symlinkSync(resolveModulePath(moduleName), moduleName, "dir");
