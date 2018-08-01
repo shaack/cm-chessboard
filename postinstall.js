d5357ecac18b48fa91216d6a4a8993b5783119e8
Stefan Haack <shaack@gmail.com>
2018-08-01 22:55:03 +0200

    now uses web-module-linker, please re-run npm install to solve dependencies

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,8 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * License: MIT, see file 'LICENSE'
+ */
+
 const WebModuleLinker = require("web-module-linker")
 
 const linker = new WebModuleLinker(__dirname)
