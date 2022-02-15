43ecfaf08494ba168a02ce471a1ea0c14f5d9bd6
shaack <shaack@gmail.com>
2022-02-16 00:37:13 +0100

    reduced code

--- src/cm-chessboard/ChessboardView.js
+++ src/cm-chessboard/ChessboardView.js
@@ -338,18 +338,10 @@ export class ChessboardView {
             this.animationRunning = true
             this.drawPieces(nextAnimation.fromSquares);
             this.currentAnimation = new ChessboardPiecesAnimation(this, nextAnimation.fromSquares, nextAnimation.toSquares, this.chessboard.props.animationDuration / (this.animationQueue.length + 1), () => {
-                if (!this.moveInput.draggablePiece) {
-                    this.animationRunning = false
-                    this.nextPieceAnimationInQueue()
-                    if (nextAnimation.callback) {
-                        nextAnimation.callback()
-                    }
-                } else {
-                    this.animationRunning = false
-                    this.nextPieceAnimationInQueue()
-                    if (nextAnimation.callback) {
-                        nextAnimation.callback()
-                    }
+                this.animationRunning = false
+                this.nextPieceAnimationInQueue()
+                if (nextAnimation.callback) {
+                    nextAnimation.callback()
                 }
             })
         }
