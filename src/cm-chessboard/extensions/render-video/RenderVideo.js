0f99a09c0e18a52cf96580067ffc472d455d8413
shaack <shaack@gmail.com>
2023-02-23 09:13:09 +0100

    copy svg to canvas works

--- src/cm-chessboard/extensions/render-video/RenderVideo.js
+++ src/cm-chessboard/extensions/render-video/RenderVideo.js
@@ -10,9 +10,62 @@ export class RenderVideo extends Extension {
 
     constructor(chessboard, props) {
         super(chessboard, props)
-        this.registerExtensionPoint(EXTENSION_POINT.animation, (data) => {
-            console.log("EXTENSION_POINT.animation", data)
+        this.images = []
+        this.makeSpriteInline()
+        this.registerExtensionPoint(EXTENSION_POINT.animation, () => {
+            // console.log("EXTENSION_POINT.animation", data)
+            let clonedSvgElement = this.chessboard.view.svg.cloneNode(true)
+            // this.makeStyleInline(clonedSvgElement)
+            let serialized = new XMLSerializer().serializeToString(clonedSvgElement)
+            // console.log(serialized)
+            const blob = new Blob([serialized], {type: 'image/svg+xml'})
+            const blobURL = URL.createObjectURL(blob)
+            const image = new Image()
+            this.images.push(image)
+            image.onload = (event) => {
+                // console.log("onload", event)
+                let context = this.canvas.getContext('2d')
+                // console.log(image)
+                context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height)
+            }
+            image.src = blobURL
         })
+        this.registerMethod("recordStart", () => {
+            let all = document.querySelectorAll("svg *")
+            for (let i = 0; i < all.length; i++) {
+                this.transferComputedStyle(all[i])
+            }
+            const dimensions = this.chessboard.view.svg.getBBox()
+            this.canvas = document.createElement("canvas")
+            this.canvas.width = dimensions.width
+            this.canvas.height = dimensions.height
+            document.body.append(this.canvas)
+        })
+    }
+
+    transferComputedStyle(element) {
+        const computedStyle = getComputedStyle(element, null)
+        for (let i = 0; i < computedStyle.length; i++) {
+            const style = computedStyle[i] + ""
+            if(style === "fill") {
+                element.style[style] = computedStyle[style]
+            }
+        }
+    }
+
+    makeSpriteInline() {
+        const wrapper = document.createElement("div")
+        wrapper.style.display = "none"
+        // wrapper.id = "wrapperId"
+        document.body.appendChild(wrapper)
+        const svg = this.chessboard.view.svg
+        const xhr = new XMLHttpRequest()
+        xhr.open("GET", this.chessboard.props.sprite.url, true)
+        xhr.onload = function () {
+            const doc = new DOMParser().parseFromString(xhr.response, "text/xml")
+            svg.prepend(doc.getElementById("Sprite-PD"))
+        }
+        xhr.send()
     }
 
 }
