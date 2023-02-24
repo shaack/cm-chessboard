905884dcbe8160cb61d12313a3cc18ecf23274b9
shaack <shaack@gmail.com>
2023-02-25 00:10:08 +0100

    optimized rendering, added promises and added "recorderPause()" method

--- src/cm-chessboard/extensions/render-video/RenderVideo.js
+++ src/cm-chessboard/extensions/render-video/RenderVideo.js
@@ -29,51 +29,58 @@ export class RenderVideo extends Extension {
         this.image = new Image()
         document.body.append(this.image)
         this.makeSpriteInline()
-        this.registerExtensionPoint(EXTENSION_POINT.animation, (event) => {
+        this.registerExtensionPoint(EXTENSION_POINT.animation, () => {
             if (this.recorder && this.recorder.state === "recording") {
                 setTimeout(() => {
                     this.cloneImageAndRender()
                 })
             }
         })
-        this.registerMethod("recorderStart", () => {
-            if (this.recorder && this.recorder.state === "recording") {
-                console.error("recorder is running")
-                return
-            }
-            let all = document.querySelectorAll("svg *")
-            for (let i = 0; i < all.length; i++) {
-                this.transferComputedStyle(all[i])
-            }
-            const dimensions = this.chessboard.view.svg.getBBox()
-            this.canvas = document.createElement("canvas")
-            this.canvas.style.backgroundColor = "#000"
-            this.canvas.style.display = "none"
-            this.canvas.width = dimensions.width
-            this.canvas.height = dimensions.height
-            this.contect = this.canvas.getContext('2d')
-
-            this.image = new Image()
-            this.image.width = this.canvas.width
-            this.image.height = this.canvas.height
-            this.image.style.position = "absolute"
-            this.image.style.visibility = "hidden"
-            document.body.append(this.image)
-
-            document.body.append(this.canvas)
-            this.stream = this.canvas.captureStream()
-            this.recorder = new MediaRecorder(this.stream, {mimeType: this.props.mediaType})
-            this.recordedData = []
-            this.recorder.ondataavailable = (event) => {
-                if (event.data && event.data.size) {
-                    this.recordedData.push(event.data)
+        this.registerMethod("recorderInit", () => {
+            return new Promise((resolve) => {
+                let all = document.querySelectorAll("svg *")
+                for (let i = 0; i < all.length; i++) {
+                    this.transferComputedStyle(all[i])
                 }
-            }
-            this.recorder.start()
-            setTimeout(() => {
-                this.cloneImageAndRender()
-            }, 50)
-            console.log("recorder", this.recorder.state)
+                const dimensions = this.chessboard.view.svg.getBBox()
+                this.canvas = document.createElement("canvas")
+                this.canvas.style.backgroundColor = "#000"
+                this.canvas.style.display = "none"
+                this.canvas.width = dimensions.width
+                this.canvas.height = dimensions.height
+                this.context = this.canvas.getContext('2d')
+                document.body.append(this.canvas)
+
+                this.image = new Image()
+                this.image.width = this.canvas.width
+                this.image.height = this.canvas.height
+                this.image.style.position = "absolute"
+                this.image.style.visibility = "hidden"
+                document.body.append(this.image)
+
+                this.stream = this.canvas.captureStream()
+                this.recorder = new MediaRecorder(this.stream, {mimeType: this.props.mediaType})
+                this.recorder.ondataavailable = (event) => {
+                    if (event.data && event.data.size) {
+                        this.recordedData.push(event.data)
+                    }
+                }
+                setTimeout(() => {
+                    resolve()
+                }, 100)
+            })
+        })
+        this.registerMethod("recorderStart", () => {
+            return new Promise((resolve) => {
+                if (this.recorder && this.recorder.state === "recording") {
+                    console.error("recorder is running")
+                    return
+                }
+                this.recordedData = []
+                this.recorder.start()
+                console.log("recorder", this.recorder.state)
+                resolve()
+            })
         })
         /**
          * returns the url of the recorded media
@@ -83,12 +90,25 @@ export class RenderVideo extends Extension {
                 if (!this.recorder || this.recorder.state !== "recording") {
                     reject("recorder is not recording")
                 }
-                this.recorder.requestData()
+                this.cloneImageAndRender()
                 setTimeout(() => {
                     this.recorder.stop()
                     console.log("recorder", this.recorder.state)
                     resolve(URL.createObjectURL(new Blob(this.recordedData, {type: this.props.mediaType})))
                 }, 200)
+            })
+        })
+        this.registerMethod("recorderPause", (ms) => {
+            return new Promise((resolve) => {
+                this.cloneImageAndRender()
+                const interval = setInterval(() => {
+                    this.cloneImageAndRender()
+                }, 20)
+                setTimeout(() => {
+                    clearInterval(interval)
+                    this.cloneImageAndRender()
+                    resolve()
+                }, ms)
             })
         })
     }
@@ -98,7 +118,8 @@ export class RenderVideo extends Extension {
         const blob = new Blob([serialized], {type: "image/svg+xml"})
         const blobURL = URL.createObjectURL(blob)
         this.image.onload = () => {
-            this.contect.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height, 0, 0, this.canvas.width, this.canvas.height)
+            this.context.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height, 0, 0, this.canvas.width, this.canvas.height)
+            this.recorder.requestData()
         }
         this.image.src = blobURL
     }
