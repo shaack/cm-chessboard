ac105e0ee74dcd65138ffde4931f9b9ec7d9b5ff
shaack <shaack@gmail.com>
2023-03-26 12:48:44 +0200

    cleaned up the PromotionDialog extension

--- src/cm-chessboard/extensions/promotion-dialog/PromotionDialog.js
+++ src/cm-chessboard/extensions/promotion-dialog/PromotionDialog.js
@@ -41,70 +41,67 @@ export class PromotionDialog extends Extension {
         if (!this.state.square) {
             return
         }
-        clearTimeout(this.redrawDialogDebounce)
-        this.redrawDialogDebounce = setTimeout(() => {
-            const squareWidth = this.chessboard.view.squareWidth
-            const squareHeight = this.chessboard.view.squareHeight
-            const squareCenterPoint = this.chessboard.view.squareToPoint(this.state.square)
-            squareCenterPoint.x = squareCenterPoint.x + squareWidth / 2
-            squareCenterPoint.y = squareCenterPoint.y + squareHeight / 2
-            while (this.promotionDialogGroup.firstChild) {
-                this.promotionDialogGroup.removeChild(this.promotionDialogGroup.firstChild)
+        const squareWidth = this.chessboard.view.squareWidth
+        const squareHeight = this.chessboard.view.squareHeight
+        const squareCenterPoint = this.chessboard.view.squareToPoint(this.state.square)
+        squareCenterPoint.x = squareCenterPoint.x + squareWidth / 2
+        squareCenterPoint.y = squareCenterPoint.y + squareHeight / 2
+        while (this.promotionDialogGroup.firstChild) {
+            this.promotionDialogGroup.removeChild(this.promotionDialogGroup.firstChild)
+        }
+        if (this.state.isShown) {
+            let turned = false
+            const rank = parseInt(this.state.square.charAt(1), 10)
+            if (this.chessboard.getOrientation() === COLOR.white && rank < 5 ||
+                this.chessboard.getOrientation() === COLOR.black && rank >= 5) {
+                turned = true
             }
-            if (this.state.isShown) {
-                let turned = false
-                const rank = parseInt(this.state.square.charAt(1), 10)
-                if (this.chessboard.getOrientation() === COLOR.white && rank < 5 ||
-                    this.chessboard.getOrientation() === COLOR.black && rank >= 5) {
-                    turned = true
-                }
-                const offsetY = turned ? -4 * squareHeight : 0
-                const offsetX = squareCenterPoint.x + squareWidth > this.chessboard.view.width ? -squareWidth : 0
-                Svg.addElement(this.promotionDialogGroup,
-                    "rect", {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y + offsetY,
-                        width: squareWidth,
-                        height: squareHeight * 4,
-                        class: "promotion-dialog"
-                    })
-                if (turned) {
-                    this.drawPieceButton(PIECE[this.state.color + "q"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y - squareHeight
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "r"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y - squareHeight * 2
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "b"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y - squareHeight * 3
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "n"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y - squareHeight * 4
-                    })
-                } else {
-                    this.drawPieceButton(PIECE[this.state.color + "q"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "r"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y + squareHeight
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "b"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y + squareHeight * 2
-                    })
-                    this.drawPieceButton(PIECE[this.state.color + "n"], {
-                        x: squareCenterPoint.x + offsetX,
-                        y: squareCenterPoint.y + squareHeight * 3
-                    })
-                }
+            const offsetY = turned ? -4 * squareHeight : 0
+            const offsetX = squareCenterPoint.x + squareWidth > this.chessboard.view.width ? -squareWidth : 0
+            Svg.addElement(this.promotionDialogGroup,
+                "rect", {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y + offsetY,
+                    width: squareWidth,
+                    height: squareHeight * 4,
+                    class: "promotion-dialog"
+                })
+            if (turned) {
+                this.drawPieceButton(PIECE[this.state.color + "q"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y - squareHeight
+                })
+                this.drawPieceButton(PIECE[this.state.color + "r"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y - squareHeight * 2
+                })
+                this.drawPieceButton(PIECE[this.state.color + "b"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y - squareHeight * 3
+                })
+                this.drawPieceButton(PIECE[this.state.color + "n"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y - squareHeight * 4
+                })
+            } else {
+                this.drawPieceButton(PIECE[this.state.color + "q"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y
+                })
+                this.drawPieceButton(PIECE[this.state.color + "r"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y + squareHeight
+                })
+                this.drawPieceButton(PIECE[this.state.color + "b"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y + squareHeight * 2
+                })
+                this.drawPieceButton(PIECE[this.state.color + "n"], {
+                    x: squareCenterPoint.x + offsetX,
+                    y: squareCenterPoint.y + squareHeight * 3
+                })
             }
-        }, 200)
+        }
     }
 
     promotionDialogOnClickPiece(event) {
@@ -119,15 +116,6 @@ export class PromotionDialog extends Extension {
         this.state.square = square
         this.state.color = color
         this.state.callback = callback
-        const mousedownListener = (event) => {
-            if (!event.target.dataset.piece && this.state.isShown) {
-                this.state.isShown = false
-                this.state.callback({square: this.state.square, piece: null})
-                this.redrawDialog()
-            }
-            this.chessboard.view.svg.removeEventListener("mousedown", mousedownListener)
-        }
-        this.chessboard.view.svg.addEventListener("mousedown", mousedownListener)
         this.redrawDialog()
     }
 
