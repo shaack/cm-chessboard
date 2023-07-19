bc7838be6b1a38a696a5b9ca364c0390dc8fba20
shaack <shaack@gmail.com>
2023-07-19 13:21:49 +0200

    added alpha version of the Persistence extension

---
+++ src/extensions/persistence/Persistence.js
@@ -0,0 +1,31 @@
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/cm-chessboard
+ * License: MIT, see file 'LICENSE'
+ */
+import {Extension, EXTENSION_POINT} from "../../model/Extension.js";
+
+export class Persistence extends Extension {
+    constructor(chessboard, props) {
+        super(chessboard)
+        console.warn("The Persistence extension is work in progress, don't use it in production.")
+        this.props = props
+        this.registerExtensionPoint(EXTENSION_POINT.positionChanged, this.savePosition.bind(this))
+        chessboard.initialized.then(() => {
+            this.loadPosition()
+        })
+    }
+
+    savePosition() {
+        localStorage.setItem("chessboard", JSON.stringify(this.chessboard.getPosition()))
+    }
+
+    loadPosition() {
+        const position = localStorage.getItem("chessboard")
+        if (position) {
+            this.chessboard.setPosition(JSON.parse(position))
+        } else {
+            this.chessboard.setPosition(this.props.initialPosition)
+        }
+    }
+}
